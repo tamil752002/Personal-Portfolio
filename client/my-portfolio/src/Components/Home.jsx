66f4cee0b2c61acfacbca56e2516071ddf85846d
tamil752002 <tamilguhan50@gmail.com>
2024-11-08 18:58:51 +0530

    add mantythings

---
+++ client/my-portfolio/src/Components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../styles/home.css"
+const Home = () => {
+  return (
+    <div  className='homemasterDiv'>
+        <div>
+        👋 Hi, I'm Tamilselvan 
+ 
+        </div>
+        <div>
+            Img 
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Home
---
+++ client/my-portfolio/src/Components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+ import "../styles/navbar.css"
+export const Navbar = () => {
+  return (
+    <div>
+        
+         <div className='navbardiv'>
+        <div>About</div>
+        <div>Project</div>
+        <div>Skills</div>
+        <div>Contact</div>
+        </div>
+         
+       
+      
+ 
+    </div>
+  )
+}
