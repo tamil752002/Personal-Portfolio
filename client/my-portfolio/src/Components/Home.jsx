b72810b7ee85f68eca3134b9e42bb42ce630fe42
tamil752002 <tamilguhan50@gmail.com>
2024-11-14 13:32:15 +0530

    added Resume

--- client/my-portfolio/src/Components/Home.jsx
+++ client/my-portfolio/src/Components/Home.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import "../styles/home.css"
-import  mYImg from "../assets/backgrdimg.jpg"
+import  mYImg from "../assets/profile.jpg"
+import resume from "../assets/Resume.pdf"
 const Home = () => {
+
+
+  const resumedowload = () => { 
+alert("jij")
+   }
   return (
     <div  id="Home" className='homemasterDiv'>
         <div className='mainContent'>
           <div className='Name' > 👋 Hi, I'm Tamilselvan   </div> 
          <div>Full stack Developer !</div>
+          <div> <button class="resume" >
+          <a href={resume} download="Tamilselvan_Resume" class="download-btn">
+               Resume
+                </a>
+         </button> </div> 
+         
  
         </div>
         <div  className='imageDiv'>
-        <img src={mYImg}  width ={ 500} alt="Background" className="backgroundImage" />
+        <img src={mYImg}  width ={ 300} alt="Background" className="backgroundImage" />
 
         </div>
 
